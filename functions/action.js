e354144e35159d4b69ee7e02defe384794360d47
afirstenberg <github@addventure.com>
2019-08-10 07:44:02 -0400

    Initial logic for rolling dice

--- functions/action.js
+++ functions/action.js
@@ -1,4 +1,5 @@
 const Multivocal = require('multivocal');
+const Util = require('multivocal/lib/util');
 
 const enWelcome = [
   "Welcome back to multi dice championship!",
@@ -22,6 +23,43 @@ const enRoll = [
   "You've rolled a {{total}}.",
   "Your total is {{total}}."
 ];
+
+/**
+ * Roll a die, generating a number from 1 to "sides" inclusive
+ * @param sides How many sides the die has
+ * @return {number} A random number between 1 and "sides" inclusive
+ */
+function rollDie( sides ){
+  return Math.ceil( Math.random() * sides );
+}
+
+/**
+ * Roll some number of dice of the same number of sides, returning the values rolled
+ * @param num How many dice to roll?
+ * @param sides How many sides should they have?
+ * @return {Array} The values of each die rolled
+ */
+function roll( num, sides ){
+  let ret = [];
+  for( let co=0; co<num; co++ ){
+    ret.push( rollDie(sides) );
+  }
+  return ret;
+}
+
+function handleRoll( env ){
+  // Roll the dice
+  let dice = roll( 10, 6 );
+  env.dice = dice;
+
+  // Add them up to get the total
+  let total = dice.reduce( (total, val) => total + val );
+  env.total = total;
+
+  // TODO: Check if this is a new high score
+
+  return Multivocal.handleDefault( env );
+}
 
 const enRank = [
   "Ranking is not yet implemented."
@@ -60,5 +98,8 @@ const conf = {
 };
 
 exports.init = function(){
+
+  Multivocal.addHandler( 'Action.roll', handleRoll );
+
   new Multivocal.Config.Simple( conf );
 };
